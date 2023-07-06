3dd057cc6aee9f88097755355f60d2c9fbdc8c4d
Ziad Fahmy <99553536+Ziad-Fahmy@users.noreply.github.com>
2023-07-06 23:39:13 +0300

    Create A. Hulk.cpp

---
+++ A. Hulk.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string feeling = "I hate ";
+    for (int i = 2; i <= n; i++) {
+        if (i % 2 == 0) {
+            feeling += "that I love ";
+        } else {
+            feeling += "that I hate ";
+        }
+    }
+    feeling += "it";
+
+    cout << feeling << endl;
+
+    return 0;
+}
